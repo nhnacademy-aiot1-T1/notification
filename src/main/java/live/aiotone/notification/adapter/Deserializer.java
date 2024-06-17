74de0ccc9debfd08f6e9cce73c2341d7a06f4112
qaw302 <osshun21@gmail.com>
2024-06-17 15:44:53 +0900

    fix : deserializer 수정

--- src/main/java/live/aiotone/notification/adapter/Deserializer.java
+++ src/main/java/live/aiotone/notification/adapter/Deserializer.java
@@ -12,8 +12,12 @@ import org.springframework.boot.jackson.JsonComponent;
 public class Deserializer extends JsonDeserializer<LocalDateTime> {
 
   @Override
-  public LocalDateTime deserialize(JsonParser p, DeserializationContext context)
-      throws IOException {
-    return LocalDateTime.parse(p.getValueAsString(), DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"));
+  public LocalDateTime deserialize(JsonParser jsonParser,
+                                   DeserializationContext deserializationContext) throws IOException {
+    DateTimeFormatter formatter = DateTimeFormatter
+            .ofPattern(
+                    "[yyyy-MM-dd HH:mm:ss][yyyy-MM-dd'T'HH:mm:ss.SSSSSS][yyyy-MM-dd'T'HH:mm:ss.SSSSS][yyyy-MM-dd'T'HH:mm:ss.SSSS][yyyy-MM-dd'T'HH:mm:ss.SSS][yyyy-MM-dd'T'HH:mm:ss.SS][yyyy-MM-dd'T'HH:mm:ss.S]");
+
+    return LocalDateTime.parse(jsonParser.getValueAsString(), formatter);
   }
 }
